c64b0ff52bd0243d9212730c9e3954cc2986c86f
munna369 <153605939+munna369@users.noreply.github.com>
2024-11-21 23:14:06 +0000

    day_3_tutorial

---
+++ pyhtonclass01/b01.py
@@ -0,0 +1,9 @@
+num1 = 19
+type(num1)
+
+print("num1 type is",type(num1))
+
+# python is a dynamic typed language
+# python data types float,tuple,complex,bool,strings etc.....
+# indentation (errors caused becuase of tab spaces which are usually 4 for python)
+#   
